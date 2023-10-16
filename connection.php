240222095165c24bc542f0b106224f2a99567160
Ceekay28 <carlkevinmagtoto@gmail.com>
2023-10-16 19:22:46 +0800

    Added Landing Page UI
    Jash - Landing Page with Login & Register

---
+++ connection.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "HelpDeskDB";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=HelpDeskDB", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e)
+{
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
---
+++ jscript.js
@@ -0,0 +1,23 @@
+const registerButton = document.getElementById("register");
+const loginButton = document.getElementById("login");
+const container = document.getElementById("container");
+
+registerButton.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
+});
+
+loginButton.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
+});
+
+document.getElementById("get-started").addEventListener("click", function(){
+    document.getElementById("popup_body").style.display = "flex";
+});
+
+document.querySelector(".close").addEventListener("click", function(){
+    document.getElementById("popup_body").style.display = "none";
+});
+
+document.querySelector(".close1").addEventListener("click", function(){
+    document.getElementById("popup_body").style.display = "none";
+});
