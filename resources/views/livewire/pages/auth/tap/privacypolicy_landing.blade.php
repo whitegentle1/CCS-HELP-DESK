ef113acd77660a3b4d2fdd4fd46fdc2d97c28bc0
Jake Ashley Panlilio <2021300973@dhvsu.edu.ph>
2023-12-05 18:53:40 +0800

    modified:   public/assets/css/styles.css 	modified:   resources/views/livewire/pages/auth/register.blade.php 	new file:   resources/views/livewire/pages/auth/tap/privacypolicy_landing.blade.php 	new file:   resources/views/livewire/pages/auth/tap/termsandconditions_landing.blade.php 	modified:   routes/web.php

---
+++ resources/views/livewire/pages/auth/tap/privacypolicy_landing.blade.php
@@ -0,0 +1,75 @@
+<x-guest-layout >
+<x-tosnpriv>
+    <div class="m-5 text-sm text-white">
+    <p class="text-3xl my-2">Privacy Policy</p>
+    <p>Last Update: Nov. 16, 2023</p>
+    <p class="ml-3 mb-3">
+        This Privacy Policy outlines the manner in which DHVSU-CCS HELP DESK
+        collects, uses, maintains, and discloses information collected from
+        users (referred to as "Students") of the DHVSU-CCS HELP DESK
+        website. This website is exclusively available for students at [Don
+        Honorio Ventura State University, College of Computing Studies] for
+        the purpose of accessing information and requesting documents.
+    </p>
+
+    <p class="text-2xl mt-2">Information Collection</p>
+    <p class="ml-3 mb-3">
+        DHVSU-CCS HELP DESK may collect personal identification information
+        from Students in various ways, including but not limited to, when
+        Students visit our site, register on the site, fill out a form, and
+        in connection with other activities, services, features, or
+        resources we make available on our website. Students may be asked
+        for, as appropriate, name, email address, contact number, and other
+        relevant details.
+    </p>
+
+    <p class="text-2xl mt-2">Use of Information</p>
+    <p class="ml-3 mb-3">
+        The information collected is used to facilitate document requests,
+        provide assistance, and enhance your experience on DHVSU-CCS HELP
+        DESK. We may also use your email address to send important updates,
+        notifications, or respond to your inquiries.
+    </p>
+
+    <p class="text-2xl mt-2">Data Security</p>
+    <p class="ml-3 mb-3">
+        We prioritize the security of your personal information and employ
+        industry-standard measures to safeguard it against unauthorized
+        access, disclosure, alteration, or destruction.
+    </p>
+
+    <p class="text-2xl mt-2">Sharing of Information</p>
+    <p class="ml-3 mb-3">
+        We do not sell, trade, or otherwise transfer your personally
+        identifiable information to outside parties. Your information is
+        shared only within DHVSU-CCS HELP DESK for the sole purpose of
+        providing requested services.
+    </p>
+
+    <p class="text-2xl mt-2">Third-Party Services</p>
+    <p class="ml-3 mb-3">
+        DHVSU-CCS HELP DESK may utilize third-party services, such as cloud
+        storage or communication tools, to enhance functionality. These
+        services have their own privacy policies, and we encourage you to
+        review them for your understanding.
+    </p>
+
+    <p class="text-2xl mt-2">Your Consent</p>
+    <p class="ml-3 mb-3">
+        By using DHVSU-CCS HELP DESK, you consent to our privacy policy and
+        agree to the collection and use of information as outlined herein.
+    </p>
+
+    <p class="text-2xl mt-2">Changes to This Privacy Policy</p>
+    <p class="ml-3 mb-3">
+        DHVSU-CCS HELP DESK reserves the right to update or modify this
+        privacy policy. Any changes will be posted on this page, and it is
+        your responsibility to review this policy periodically.
+    </p>
+    <p class="ml-3 mb-3">
+        By using DHVSU-CCS HELP DESK, you acknowledge that you have read and
+        understood this privacy policy and agree to its Terms of Use.
+    </p>
+</div>
+</div>
+</x-tosnpriv></x-guest-layout>
--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,13 @@ Route::post('/email/verification-notification', function (Request $request) {
     return back()->with('message', 'Verification link sent!');
 })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
+Route::get('/termsandconditions&landing', function () {
+    return view('livewire.pages.auth.tap.termsandconditions_landing');
+})->name('tac&landing');
+
+Route::get('/privacypolicy&landing', function () {
+    return view('livewire.pages.auth.tap.privacypolicy_landing');
+})->name('pp&landing');
 
 require __DIR__ . '/auth.php';
 
